301f43e7a4911a67b10ef93dd42e03c54154018b
Alexey Shevchenko <kamikozz@mail.ru>
2021-01-06 02:59:42 +0300

    [add] POST /twitch which resends this JSON data to the Discord Webhook

--- worker.js
+++ worker.js
@@ -1,15 +1,49 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+const https = require('https');
+
 const app = express();
+app.use(bodyParser.urlencoded({
+  extended: true,
+}));
+app.use(bodyParser.json());
+
 const PORT = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
 });
 
-app.get('/twitch', () => {
-  
+const sendToDiscord = (json) => {
+  const options = {
+    hostname: 'discord.com',
+    path: '',
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  };
+  const req = https.request(options);
+  req.write(JSON.stringify({
+    "content": JSON.stringify(json),
+    "avatar_url": 'https://picsum.photos/200/300',
+  }));
+  req.end();
+};
+
+app.post('/twitch', (req, res) => {
+  console.log(req.body);
+  res.status(200).send('OK');
+  sendToDiscord(req.body);
 });
 
+// {
+//   "hub.callback": "https://discord.com",
+//   "hub.mode": "subscribe",
+//   "hub.topic": "https://api.twitch.tv/helix/streams?user_id=",
+//   "hub.lease_seconds": 864000
+// }
+
 app.listen(PORT, () => {
-  console.log(`Example app listening...${PORT}`);
+  console.log(`App listening...${PORT}`);
 });
