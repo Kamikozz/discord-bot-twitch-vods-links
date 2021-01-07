26c5dc61d40dd82a5f3d891b6ee75b78dd2f959a
Alexey Shevchenko <kamikozz@mail.ru>
2021-01-07 06:31:41 +0300

    [add] format thumbnail_url into vodUrl & previewUrl

--- worker.js
+++ worker.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const https = require('https');
+const credentials = require('./credentials');
 
 const app = express();
 app.use(bodyParser.urlencoded({
@@ -14,27 +15,16 @@ app.get('/', (req, res) => {
   res.send('Hello World!');
 });
 
-const sendToDiscord = (json) => {
-  const options = {
-    hostname: 'discord.com',
-    path: '',
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json',
-    },
-  };
-  const req = https.request(options);
-  req.write(JSON.stringify({
-    "content": JSON.stringify(json),
-    "avatar_url": 'https://picsum.photos/200/300',
-  }));
-  req.end();
-};
-
 app.post('/twitch', (req, res) => {
   console.log(req.body);
   res.status(200).send('OK');
   sendToDiscord(req.body);
+  if (req.body.data.length) {
+    console.log('Stream Change Events');
+  } else {
+    console.log('Stream Offline Event');
+    getUserVideos();
+  }
 });
 
 app.get('/twitch', (req, res) => {
@@ -57,3 +47,55 @@ app.get('/twitch', (req, res) => {
 app.listen(PORT, () => {
   console.log(`App listening...${PORT}`);
 });
+
+const getUserVideos = (userId = '') => {
+  const options = {
+    hostname: 'api.twitch.tv',
+    path: `/helix/videos?user_id=${userId}`,
+    method: 'GET',
+    headers: {
+      'Authorization': `Bearer ${credentials.twitch.token}`,
+      'Client-Id': credentials.twitch.clientId,
+    },
+  };
+  let responseData = '';
+  const req = https.get(options, (res) => {
+    res.on('data', (chunk) => {
+      responseData += chunk;
+    });
+    res.on('end', () => {
+      const parsedObj = JSON.parse(responseData);
+      const lastVideo = parsedObj.data[0];
+      const mappedObj = {
+        title: lastVideo.title,
+        thumbnail_url: lastVideo.thumbnail_url,
+      };
+      const discordObj = {
+        title: mappedObj.title,
+        imageUrl: mappedObj.thumbnail_url
+          .replace('%{width}', '600')
+          .replace('%{height}', '350'),
+        vodUrl: `https://vod-secure.twitch.tv/${mappedObj.thumbnail_url.split('/')[5]}/chunked/index-dvr.m3u8`,
+      };
+      console.dir(discordObj);
+      sendToDiscord(discordObj);
+    });
+  });
+};
+
+const sendToDiscord = (json) => {
+  const options = {
+    hostname: 'discord.com',
+    path: '',
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  };
+  const req = https.request(options);
+  req.write(JSON.stringify({
+    "content": JSON.stringify(json),
+    "avatar_url": 'https://picsum.photos/200/300',
+  }));
+  req.end();
+};
